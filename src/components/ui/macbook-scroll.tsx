424767c8da5b5c1ec990c8322c80ca29d24c8d91
Bennett B. Walter <bennett.b.walter@gmail.com>
2026-01-26 21:09:23 +0000

    Fix mobile laptop screen stretch issue

--- src/components/ui/macbook-scroll.tsx
+++ src/components/ui/macbook-scroll.tsx
@@ -55,12 +55,12 @@ export const MacbookScroll = ({
     const scaleX = useTransform(
         scrollYProgress,
         [0, 0.3],
-        [1.2, isMobile ? 1 : 1.5],
+        [isMobile ? 1 : 1.2, isMobile ? 1 : 1.5],
     );
     const scaleY = useTransform(
         scrollYProgress,
         [0, 0.3],
-        [0.6, isMobile ? 1 : 1.5],
+        [isMobile ? 1 : 0.6, isMobile ? 1 : 1.5],
     );
     const translate = useTransform(scrollYProgress, [0, 1], [0, 1500]);
     const rotate = useTransform(scrollYProgress, [0.1, 0.12, 0.3], [-28, -28, 0]);
