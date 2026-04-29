cab6358c44f125cc1828797856a88d5249c93f7b
Bennett B. Walter <bennett.b.walter@gmail.com>
2026-04-29 23:20:08 +0100

    chore: update sitemaps

---
+++ .wrangler/tmp/pages-VZKEG9/functionsRoutes-0.607406801164238.mjs
@@ -0,0 +1,11 @@
+import { onRequest as ___middleware_ts_onRequest } from "C:\\Users\\Nick\\Downloads\\hitmaker-2026\\emergency-tradesmen\\functions\\_middleware.ts"
+
+export const routes = [
+    {
+      routePath: "/",
+      mountPath: "/",
+      method: "",
+      middlewares: [___middleware_ts_onRequest],
+      modules: [],
+    },
+  ]
